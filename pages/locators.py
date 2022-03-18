0e08e5101ea7458be6a81431c78eda1be8333330
Andrey <aws110615@gmail.com>
2022-03-18 12:47:48 +0500

    Add functional for Product Page.

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRICE_PRODUCT_IN_ALERTINNER = (By.CSS_SELECTOR, ".alertinner > p > strong")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main > h1")
+    NAME_PRODUCT_IN_ALERTINNER = (By.CSS_SELECTOR, ".alertinner > strong")
---
+++ pages/product_page.py
@@ -0,0 +1,33 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def click_button_add_to_basket(self):
+        login_link = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        login_link.click()
+        #alert = self.browser.switch_to.alert
+        #alert.accept()
+
+    def should_be_button_add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_BASKET), \
+            "Button add to basket is not presented!"
+
+    def should_be_elements_for_product(self):
+        assert self.is_element_present(*ProductPageLocators.NAME_PRODUCT), \
+            "Name product is not presented!"
+        assert self.is_element_present(*ProductPageLocators.PRICE_PRODUCT), \
+            "Price product is not presented!"
+
+    def should_be_elements_for_alert_inner(self):
+        assert self.is_element_present(*ProductPageLocators.NAME_PRODUCT_IN_ALERTINNER), \
+            "Name product in alert inner is not presented!"
+        assert self.is_element_present(*ProductPageLocators.PRICE_PRODUCT_IN_ALERTINNER), \
+            "Price product in alert inner is not presented!"
+
+    def ckeck_data_product_and_alert_inner(self):
+        nameProduct = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+        nameProductInAlertInner = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_IN_ALERTINNER).text
+        priceProduct = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
+        priceProductInAlertInner = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_IN_ALERTINNER).text
+        self.s1_equals_s2(nameProduct, nameProductInAlertInner)
+        self.s1_equals_s2(priceProduct, priceProductInAlertInner)
